4454afc57a7c456029ab9022fd9deb3ba707afda
enrique.vidal <enrique.vidal@f22fe770-7171-0410-8750-96b5d1fa6569>
2009-12-31 21:40:53 +0000

    Simplified way to generate page names, the user now only needs to set the title and the page name will be automatically generated
    git-svn-id: svn+ssh://thinktankhost.com/cloverinteractive/trunk@41 f22fe770-7171-0410-8750-96b5d1fa6569

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,19 +1,20 @@
 class ViewerController < ApplicationController
 
   layout 'website'
- 
+
   def home_page
     redirect_to show_section_page_path( :section_name => Section.first.name, :page_name => Section.first.pages.first.name )
   end
- 
+
   def show_section_page
     @section  = Section.find_by_name(params[:section_name])
     @page     = @section.pages.find_by_name(params[:page_name])
   end
-  
+
   def show_subsection_page
     @section    = Section.find_by_name(params[:section_name])
     @subsection = @section.subsections.find_by_name(params[:subsection_name])
     @page       = @subsection.pages.find_by_name(params[:page_name])
   end
 end
+
--- app/models/page.rb
+++ app/models/page.rb
@@ -3,17 +3,29 @@ class Page < ActiveRecord::Base
 
   belongs_to  :section
   belongs_to  :subsection
-  has_one     :meta_title,         :class_name => 'MetaTag', :foreign_key => :id
-  has_one     :meta_description,   :class_name => 'MetaTag', :foreign_key => :id
+  belongs_to  :meta_title,         :class_name => 'MetaTag'
+  belongs_to  :meta_description,   :class_name => 'MetaTag'
 
   has_many    :uploads, :dependent => :destroy, :attributes => true, :discard_if => proc { |upload| upload.description.blank? }
 
-  validates_presence_of   :title, :body, :name
+  validates_presence_of   :title, :body
   validates_uniqueness_of :title, :name
-  validates_format_of     :name, :with => /^[a-zA-Z0-9_-]+$/
+
+  before_create :create_page_name, :add_metatags
+  before_update :create_page_name, :add_metatags
 
   cattr_reader :per_page
   @@per_page = 5
+
+  def add_metatags
+    self.meta_title_id        = MetaTag.first.id unless !self.meta_title_id.nil?
+    self.meta_description_id  = MetaTag.first.id unless !self.meta_description_id.nil?
+  end
+
+  def create_page_name
+    title  = self.title.gsub(/\s{2,}/, ' ').gsub(/\t/, ' ')
+    self.name   = title.downcase.gsub(/(\s|\t)+/, '_').gsub(/_{2,}/, '_').gsub(/[^a-z_]/, '')
+  end
 
 end
 
