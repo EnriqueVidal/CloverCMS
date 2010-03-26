feb834f78e1c1d3fa8401fe0e5833046b4444f2b
Enrique Vidal <enrique@cloverinteractive.com>
2010-03-26 12:45:55 -0800

    Added exception for when tlsmail is not yet installed

--- config/environment.rb
+++ config/environment.rb
@@ -54,21 +54,22 @@ Rails::Initializer.run do |config|
 
 end
 
+begin  
+  require 'tlsmail'
 
-require 'tlsmail'
+  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 
-Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-
-ActionMailer::Base.delivery_method        = :smtp
-ActionMailer::Base.perform_deliveries     = true
-ActionMailer::Base.default_charset        = "utf-8"
-ActionMailer::Base.raise_delivery_errors  = true
-ActionMailer::Base.smtp_settings          = {
-                                              :domain          => "cloverinteractive.com",
-                                              :address         => 'smtp.gmail.com',
-                                              :port            => 587,
-                                              :tls             => true,
-                                              :authentication  => :plain,
-                                              :user_name       => 'dont-reply@cloverinteractive.com',
-                                              :password        => 'H@Nn@L1v3$C10v3R1N73r@kT1v3'
-                                            }
+  ActionMailer::Base.delivery_method        = :smtp
+  ActionMailer::Base.perform_deliveries     = true
+  ActionMailer::Base.default_charset        = "utf-8"
+  ActionMailer::Base.raise_delivery_errors  = true
+  ActionMailer::Base.smtp_settings          = {
+                                                :domain          => "cloverinteractive.com",
+                                                :address         => 'smtp.gmail.com',
+                                                :port            => 587,
+                                                :tls             => true,
+                                                :authentication  => :plain,
+                                                :user_name       => 'dont-reply@cloverinteractive.com',
+                                                :password        => 'H@Nn@L1v3$C10v3R1N73r@kT1v3'
+                                              }
+end rescue "tlsmail not yet installed"
