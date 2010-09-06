6c0b4b8ae0adfbacf0182b8f24bac31b96a4251a
Enrique Vidal <enrique@cloverinteractive.com>
2010-09-06 00:40:53 -0700

    Added default host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,4 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+config.action_mailer.default_url_options = { :host => 'www.cloverinteractive.com' }
