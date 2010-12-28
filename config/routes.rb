d8fdfc9a5727d727a018d23beaba64bd432e5f6b
Enrique Vidal <enrique@cloverinteractive.com>
2010-12-27 23:33:31 -0800

    Cleaned up routes and remove un-needed files

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Clover::Application.routes.draw do
   devise_for :users
 
-  match ":section_name/:page_name.html",                  :to => "pages#show", :as => :section_page
-  match ":section_name/:subsection_name/:page_name.html", :to => "pages#show", :as => :subsection_page
+  match ":section_name/:page_name.html",                  :to => "pages#show", :as => :section_page,    :via => :get
+  match ":section_name/:subsection_name/:page_name.html", :to => "pages#show", :as => :subsection_page, :via => :get
 
   resources :assets
-  resources :articles
-  resources :authentications, :only => [ :index, :create, :destroy ]
-
+  resources :articles,  :only   => [ :create, :destroy ]
   resources :sections,  :except => :show  do
     resources :pages,   :except => :show
   end
 
-  root :to => "pages#show", :home_page => true
+  root :to => "pages#show", :home_page => true, :via => :get
 end
